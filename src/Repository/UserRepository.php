b34c4126f2707a583e6faefb92d86755b61f511b
Pierre LEJEUNE <darkanakin41@gmail.com>
2019-12-31 18:24:09 +0100

    Correction of the Registry used

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,7 +4,7 @@ namespace Darkanakin41\UserBundle\Repository;
 
 use Darkanakin41\UserBundle\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,12 +14,13 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class UserRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
 
-    public function findFromCredentials($username){
+    public function findFromCredentials($username)
+    {
         $qb = $this->createQueryBuilder("u");
         $qb->where("(u.username = :username OR u.email = :username)");
         $qb->setParameter("username", $username);
@@ -27,10 +28,11 @@ class UserRepository extends ServiceEntityRepository
         return $qb->getQuery()->getOneOrNullResult();
     }
 
-    public function findFromRole($role){
+    public function findFromRole($role)
+    {
         $qb = $this->createQueryBuilder("u");
         $qb->where($qb->expr()->like("u.roles", ":role"));
-        $qb->setParameter("role", "%" . $role . "%");
+        $qb->setParameter("role", "%".$role."%");
 
         return $qb->getQuery()->getResult();
     }
